cfe4621dbb9fec4184be9dca1e767bd6c4cb1ca8
syrinemahmoud <56481359+syrinemahmoud@users.noreply.github.com>
2019-11-13 20:45:06 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from './form';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
+    
+    
+
+
+  
+
+
